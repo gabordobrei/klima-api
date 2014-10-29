23290e8e8e0613ab77ee6faeb8b27b167bab9baf
Döbrei Gábor <dobrei.gabor@nextent.hu>
2014-10-29 14:21:31 +0100

    AccidentData model upgraded to more complex structure

--- migrations/20141023095300-accident-data-migration.js
+++ migrations/20141023095300-accident-data-migration.js
@@ -8,23 +8,43 @@ module.exports = {
             primaryKey:    true,
             autoIncrement: true
           },
- 
+
           happenedAt: {
             type: Sequelize.DATE
           },
-          
+
           outcome: {
             type:      Sequelize.ENUM,
             values:    ['minor', 'major', 'lethal'],
             allowNull: false
           },
+
+          roadShape: {
+            type: Sequelize.ENUM,
+            values: ['straight', 'bend', 'bends', 'bump', 'crossroads' ]
+          },
+
           latitude:  Sequelize.FLOAT(14, 12),
-          longitude: Sequelize.FLOAT(14, 12)
+          longitude: Sequelize.FLOAT(14, 12),
+
+          isBuiltupAreas:                 Sequelize.BOOLEAN,
+          isDirectionFrontal:             Sequelize.BOOLEAN,
+          isDirectionSameDirection:       Sequelize.BOOLEAN,
+          isDirectionStraightAndStraight: Sequelize.BOOLEAN,
+          isDirectionStraightAndCrossing: Sequelize.BOOLEAN,
+
+          isPassangerInjured:  Sequelize.BOOLEAN,
+          isPedestrianInjured: Sequelize.BOOLEAN,
+
+          withTrain:      Sequelize.BOOLEAN,
+          withWildAnimal: Sequelize.BOOLEAN,
+          withPet:        Sequelize.BOOLEAN,
+          isOtherCause:   Sequelize.BOOLEAN
         }
       )
       .complete(done);
   },
- 
+
   down: function (migration, Sequelize, done) {
     migration
       .dropTable('AccidentData')
--- models/accident-data.js
+++ models/accident-data.js
@@ -1,19 +1,41 @@
 var path = require('path');
- 
+
 module.exports = function (sequelize, Sequelize) {
- 
+
   var AccidentData = sequelize.define('AccidentData', {
-    
+
     happenedAt: Sequelize.DATE,
+
     outcome: {
       type:   Sequelize.ENUM,
       values: ['minor', 'major', 'lethal']
     },
+
+    roadShape: {
+      type: Sequelize.ENUM,
+      values: ['straight', 'bend', 'bends', 'bump', 'crossroads' ]
+    },
+
     latitude:  Sequelize.FLOAT(14, 12),
-    longitude: Sequelize.FLOAT(14, 12)
+    longitude: Sequelize.FLOAT(14, 12),
+
+    isBuiltupAreas:                 Sequelize.BOOLEAN,
+    isDirectionFrontal:             Sequelize.BOOLEAN,
+    isDirectionSameDirection:       Sequelize.BOOLEAN,
+    isDirectionStraightAndStraight: Sequelize.BOOLEAN,
+    isDirectionStraightAndCrossing: Sequelize.BOOLEAN,
+
+    isPassangerInjured:  Sequelize.BOOLEAN,
+    isPedestrianInjured: Sequelize.BOOLEAN,
+
+    withTrain:      Sequelize.BOOLEAN,
+    withWildAnimal: Sequelize.BOOLEAN,
+    withPet:        Sequelize.BOOLEAN,
+    isOtherCause:   Sequelize.BOOLEAN
+
   }, {
     timestamps: false
   });
- 
+
   return AccidentData;
 };
